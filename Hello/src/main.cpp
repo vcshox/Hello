30232191ca2aff729a4d15d052d6a2fd7e831bfd
vcshox <vcshox520@gmail.com>
2015-07-15 16:00:10 +0800

    Hello World

--- Hello/src/main.cpp
+++ Hello/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 main::main()
 {
     //ctor
@@ -8,4 +9,10 @@ main::main()
 main::~main()
 {
     //dtor
+}
+
+int main( ){
+
+ printf("Hello!");
+
 }
